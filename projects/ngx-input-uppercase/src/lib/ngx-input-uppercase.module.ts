383f989aaa304592b83179fc5b60fa84dc9d980d
belmirofss <belmirofss@gmail.com>
2020-08-30 20:02:03 -0300

    first commit

---
+++ projects/ngx-input-uppercase/src/lib/ngx-input-uppercase.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxInputUppercaseDirective } from './ngx-input-uppercase.directive';
+
+@NgModule({
+  declarations: [
+    NgxInputUppercaseDirective
+  ],
+  exports: [
+    NgxInputUppercaseDirective
+  ]
+})
+export class NgxInputUppercaseModule { }
---
+++ projects/ngx-input-uppercase/src/public-api.ts
@@ -0,0 +1,1 @@
+export * from './lib/ngx-input-uppercase.module';
